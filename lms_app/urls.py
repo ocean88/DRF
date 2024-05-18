a502ca27480cc43365bc0c9c6ade8a0eb0fff1c2
ocean88 <oceann3@gmail.com>
2024-05-18 16:34:21 +0900

    исправлены замечания

--- lms_app/urls.py
+++ lms_app/urls.py
@@ -1,37 +1,37 @@
 from lms_app.apps import LmsAppConfig
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from lms_app.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+from lms_app.views import CourseViewSet, LessonCreateAPIView, \
+    LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = LmsAppConfig.name
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 urlpatterns = [
-    path(
-     'lesson/create/',
-     LessonCreateAPIView.as_view(),
-     name='lesson-create'
-    ),
-    path(
-     'lessons/',
-     LessonListAPIView.as_view(),
-     name='lesson-list'
-    ),
-    path(
-     'lesson/<int:pk>/',
-     LessonRetrieveAPIView.as_view(),
-     name='lesson-view'
-    ),
-    path(
-     'lesson/<int:pk>/update/',
-     LessonUpdateAPIView.as_view(),
-     name='lesson-update'
-    ),
-    path(
-     'lesson/<int:pk>/delete/',
-     LessonDestroyAPIView.as_view(),
-     name='lesson-delete'
-    ),
+                  path(
+                      'lesson/create/',
+                      LessonCreateAPIView.as_view(),
+                      name='lesson-create'
+                  ),
+                  path(
+                      'lessons/',
+                      LessonListAPIView.as_view(),
+                      name='lesson-list'
+                  ),
+                  path(
+                      'lesson/<int:pk>/',
+                      LessonRetrieveAPIView.as_view(),
+                      name='lesson-view'
+                  ),
+                  path(
+                      'lesson/<int:pk>/update/',
+                      LessonUpdateAPIView.as_view(),
+                      name='lesson-update'
+                  ),
+                  path(
+                      'lesson/<int:pk>/delete/',
+                      LessonDestroyAPIView.as_view(),
+                      name='lesson-delete'
+                  ),
               ] + router.urls
